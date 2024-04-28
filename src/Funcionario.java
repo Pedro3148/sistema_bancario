eb0e55ff0911ee4b448c295d987975a3c55c07e2
Pedro3148 <pedrohenrique3148@gmail.com>
2024-04-28 01:25:46 -0300

    update Funcionario

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -5,11 +5,13 @@ public class Funcionario extends Thread{
     private final Conta contaSalario;
     private final Conta contaInvestimento;
     private final Banco banco;
+    private boolean esperandoPagamento;
 
     Funcionario(String nome, Loja loja, Banco banco) {
         super(nome);
         this.loja = loja;
         this.banco = banco;
+        this.esperandoPagamento = true;
 
         contaSalario = new Conta(getName() + " Salario", 0.00);
         contaInvestimento = new Conta(getName() + " Investimento", 0.00);
@@ -20,18 +22,19 @@ public class Funcionario extends Thread{
     }
 
     public void run() {
-        int numRandom = new Random().nextInt(4000) + 1000;
+        while (esperandoPagamento) {
+            int numRandom = new Random().nextInt(4000) + 1000;
+            try {
+                Thread.sleep(numRandom);
+            } catch (InterruptedException e) {
+                System.out.println("Thread foi interrompida!");
+            }
 
-        try {
-            Thread.sleep(numRandom);
-        } catch (InterruptedException e) {
-            System.out.println("Thread foi interrompida!");
+            if (loja.pagarFuncionario(this.contaSalario)) {
+                Double valorParaInvestir = this.contaSalario.getSaldo() * 0.2;
+                investir(valorParaInvestir);
+            }
         }
-
-        loja.pagarFuncionario(this.contaSalario);
-
-        Double valorParaInvestir = this.contaSalario.getSaldo() * 0.2;
-        investir(valorParaInvestir);
     }
 
     public void investir(Double valor) {
@@ -54,4 +57,7 @@ public class Funcionario extends Thread{
         return contaInvestimento;
     }
 
+    public void mudarStatus() {
+        this.esperandoPagamento = false;
+    }
 }
