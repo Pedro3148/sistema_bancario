6a1b82672db7aa96ad3dad751d82aa23ca0a5b74
Pedro3148 <pedrohenrique3148@gmail.com>
2024-04-27 21:47:12 -0300

    Update Loja

--- src/Loja.java
+++ src/Loja.java
@@ -1,28 +1,48 @@
+import java.util.Random;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Loja {
     private ReentrantLock lock;
+    private Condition saldoDisponivelPagamento;
 
-    private String nome;
-    private Conta conta;
-    private Banco banco;
+    private final String nome;
+    private final Conta conta;
+    private final Banco banco;
+    private final Double salarioFuncionario;
 
     Loja(String nome, Banco banco) {
         this.lock = new ReentrantLock();
-
+        this.saldoDisponivelPagamento = this.lock.newCondition();
         this.nome = nome;
         this.banco = banco;
+        this.salarioFuncionario = 1400.00;
 
         conta = new Conta(this.nome, 0.00);
         banco.adicionarConta(conta);
     }
 
-    public void pagarFuncionario(Conta conta) {
+    public void comprar(Cliente cliente, Double valorCompra) {
+        this.lock.lock();
+        banco.transferir(cliente.getConta(), this.conta, valorCompra);
+        if (this.conta.getSaldo() >= this.salarioFuncionario) {
+            this.saldoDisponivelPagamento.signalAll();
+        }
+        this.lock.unlock();
+    }
+
+    public void pagarFuncionario(Conta contaFuncionario) {
         this.lock.lock();
 
-        if (this.conta.getSaldo() >= 1400) {
-            banco.transferir(this.conta, conta, 1400.00);
+        while (this.conta.getSaldo() < this.salarioFuncionario) {
+            try {
+                this.saldoDisponivelPagamento.await();
+            } catch (InterruptedException e) {
+                System.out.println("Thread foi interrompida!");
+            }
         }
+
+        banco.transferir(this.conta, contaFuncionario, this.salarioFuncionario);
         this.lock.unlock();
     }
 
@@ -37,5 +57,9 @@ public class Loja {
 
     public String getNome() {
         return nome;
+    }
+
+    public Double getSalarioFuncionario() {
+        return salarioFuncionario;
     }
 }
