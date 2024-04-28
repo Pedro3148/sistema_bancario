631347098aa67e0b375f98cf89fba2692b9176cd
Pedro3148 <pedrohenrique3148@gmail.com>
2024-04-27 21:46:39 -0300

    Update Cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -2,9 +2,9 @@ import java.util.List;
 import java.util.Random;
 
 public class Cliente extends Thread{
-    private Conta conta;
-    private Banco banco;
-    private List<Loja> lojas;
+    private final Conta conta;
+    private final Banco banco;
+    private final List<Loja> lojas;
 
     Cliente(String nome, List<Loja> lojas, Banco banco) {
         super(nome);
@@ -18,25 +18,21 @@ public class Cliente extends Thread{
 
     public void run() {
         while (conta.getSaldo() != 0) {
+            int numRandom = new Random().nextInt(4000) + 1000;
+
+            try {
+                Thread.sleep(numRandom);
+            } catch (InterruptedException e) {
+                System.out.println("Thread foi interrompida!");
+            }
+
             Loja loja = lojas.get(new Random().nextInt(lojas.size()));
 
-            int numRandom = new Random().nextInt(2);
-            Double valorCompra = numRandom == 0 ? 100.00 : 200.00;
+            int numRandomValor = new Random().nextInt(2);
+            Double valorCompra = numRandomValor == 0 ? 100.00 : 200.00;
 
-            comprar(loja, valorCompra);
+            loja.comprar(this, valorCompra);
         }
-    }
-
-    public void comprar(Loja loja, Double valorCompra) {
-        int numRandom = new Random().nextInt(4000) + 1000;
-
-        try {
-            Thread.sleep(numRandom);
-        } catch(InterruptedException e) {
-            System.out.println("Thread foi interrompida!");
-        }
-
-        banco.transferir(this.conta, loja.getConta(), valorCompra);
     }
 
     public Conta getConta() {
